efe245787a6590f49aac98c028b42891c880f0f7
imterah <me@terah.dev>
2025-05-06 07:48:49 -0400

    feature: Adds base Traefik stack

--- hosts/andromeda/configuration.nix
+++ hosts/andromeda/configuration.nix
@@ -14,6 +14,9 @@
     ../../system/sshd.nix
     ../../system/avahifixes.nix
     ../../system/i18n.nix
+
+    # Docker stacks
+    ./andromeda/stacks/traefik/docker-compose.nix
   ];
 
   users.mutableUsers = false;
@@ -32,6 +35,10 @@
       "networkmanager"
     ];
 
+    packages = with pkgs; [
+      git
+    ];
+
     openssh.authorizedKeys.keys = [
       (builtins.readFile ../../data/id_user.pub)
     ];
@@ -46,11 +53,26 @@
   # Services
   virtualisation.docker = {
     enable = true;
+    autoPrune.enable = true;
+
     storageDriver = "btrfs";
+  };
+
+  virtualisation.oci-containers.backend = "docker";
+
+  # Volumes
+  fileSystems."/mnt/NASBox" = {
+    device = "192.168.0.3:/mnt/Diskette/KubeData";
+    fsType = "nfs";
   };
 
   environment.systemPackages = builtins.attrValues {
     inherit (pkgs) htop btop micro nano;
+  };
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [80 443 8000];
   };
 
   system.stateVersion = "24.11";
