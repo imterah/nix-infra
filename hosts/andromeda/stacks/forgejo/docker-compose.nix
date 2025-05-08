0458af952e77675d2be964acf0a2202c96b4bb29
imterah <me@terah.dev>
2025-05-07 21:23:32 -0400

    fix: Fix captcha type being incorrect for Forgejo

--- hosts/andromeda/stacks/forgejo/docker-compose.nix
+++ hosts/andromeda/stacks/forgejo/docker-compose.nix
@@ -90,7 +90,7 @@
       "FORGEJO__service__REGISTER_EMAIL_CONFIRM" = "false";
       "FORGEJO__service__REQUIRE_CAPTCHA_FOR_LOGIN" = "true";
       "FORGEJO__service__MCAPTCHA_URL" = "https://mcaptcha.terah.dev";
-      "FORGEJO__service__CAPTCHA_TYPE" = "CAPTCHA_TYPE";
+      "FORGEJO__service__CAPTCHA_TYPE" = "mcaptcha";
       "FORGEJO__service__ENABLE_CAPTCHA" = "true";
       "FORGEJO__service__REGISTER_MANUAL_CONFIRM" = "true";
       "FORGEJO__service__REQUIRE_EXTERNAL_REGISTRATION_CAPTCHA" = "true";
